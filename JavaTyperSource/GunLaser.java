70954a86f3b54a095468891cadd82d1dee26f10c
Josh Beatty <joshuatb6@gmail.com>
2019-09-20 15:07:53 -0700

    Update GunLaser.java

--- JavaTyperSource/GunLaser.java
+++ JavaTyperSource/GunLaser.java
@@ -1,21 +1,24 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class GunLaser here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * The gunLaser Class just moevs itself foward
+ * Its main purpose is to allow for itself to be created to collide with an asteroid
+ *
+ * @author Josh Beatty
+ * @version 2019-09-18
  */
 public class GunLaser extends Actor
 {
-    /**
-     * Act - do whatever the GunLaser wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     private int speed = 30;
+    /**
+     * When created set its rotation to the passed parameter
+     */
     public GunLaser(int rotation){
         setRotation(rotation);
     }
+    /**
+     * Move foward at the set frame
+     */
     public void act() 
     {
         move(speed);
