6b637cbcefe15bab5ddd1ee9141b08f86a81acbe
Josh Beatty <joshuatb6@gmail.com>
2019-09-20 15:03:23 -0700

    Update GunBase.java

--- JavaTyperSource/GunBase.java
+++ JavaTyperSource/GunBase.java
@@ -1,19 +1,12 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Gun here.
+ * The gunBase class is an empty actor made to display an image
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Josh Beatty
+ * @version 2019-09-18
  */
 public class GunBase extends Actor
 {
-    /**
-     * Act - do whatever the Gun wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
-        // Add your action code here.
-    }    
+      
 }
