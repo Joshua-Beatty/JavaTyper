2e32e0ebee0819475b2d754bf516235a6431e408
vikas-u <32949284+vikas-u@users.noreply.github.com>
2019-09-20 20:07:38 -0700

    updated javadocs

--- JavaTyperSource/AsteroidText.java
+++ JavaTyperSource/AsteroidText.java
@@ -1,27 +1,26 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class AsteroidText here.
+ * AsteroidText displays the text in a readable format to move with the asteroids. 
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Vikas Ummadisetty
+ * @version 9/20/19
  */
 public class AsteroidText extends Actor
-{
-    /**
-     * Act - do whatever the AsteroidText wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    
+{    
     private String word;
-    
-    
+    /**
+     * Constructor for AsteroidText which sets word member variable and sets image for displaying text
+     */
     AsteroidText(String s) {
         word = s;
         GreenfootImage textDisplay = new GreenfootImage(word, 50, Color.WHITE, Color.BLACK, Color.BLACK);
         setImage(textDisplay);
     }
     
+    /**
+     * Does not do anything in act. All movements are in Asteroid class
+     */
     public void act() 
     {
        
